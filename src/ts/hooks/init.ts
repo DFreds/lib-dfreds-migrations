1575a19624fda14931e620fe071f085a1581fb7c
Derek Fredrickson <dfreds08@gmail.com>
2025-11-12 13:00:30 -0500

    Remove unnecessary files

--- src/ts/hooks/init.ts
+++ src/ts/hooks/init.ts
@@ -1,6 +1,3 @@
-import { ThisModule } from "../api.ts";
-import { MODULE_ID } from "../constants.ts";
-import { HandlebarHelpers } from "../handlebar-helpers.ts";
 import { Settings } from "../settings.ts";
 import { Listener } from "./index.ts";
 
@@ -8,13 +5,6 @@ const Init: Listener = {
     listen(): void {
         Hooks.once("init", () => {
             new Settings().register();
-            new HandlebarHelpers().register();
-
-            (game.modules.get(MODULE_ID) as ThisModule).api = {
-                test(): void {
-                    console.log("Cool");
-                },
-            };
         });
     },
 };

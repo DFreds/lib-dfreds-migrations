4a246eb797ce0396d8c766b260ad77ae5ea10291
Derek Fredrickson <dfreds08@gmail.com>
2026-04-07 16:53:41 -0400

    Update sample migrations

--- src/ts/sample.ts
+++ src/ts/sample.ts
@@ -1,49 +1,42 @@
+import { MODULE_ID } from "./constants.ts";
+
 export function mySampleMigrationModule(): void {
-    // @ts-expect-error Type mismatch
-    Hooks.once("migrations.setup", (migrations: Migrations) => {
-        testMigration({ moduleId: "my-sample-migration-module", migrations });
-        testMigration({ moduleId: "my-sample-migration-module-2", migrations });
-    });
+    Hooks.once("migrations.setup", (migrations: any) => {
+        const migrationsTyped = migrations as Migrations;
+        migrationsTyped.register({ moduleId: MODULE_ID });
 
-    // @ts-expect-error Type mismatch
-    Hooks.once("migrations.ready", async (migrations: Migrations) => {
-        await migrations.clearAllRan({
-            moduleId: "my-sample-migration-module",
-        });
-        await migrations.runAll({ moduleId: "my-sample-migration-module" });
-
-        await migrations.clearAllRan({
-            moduleId: "my-sample-migration-module-2",
-        });
-        await migrations.runAll({ moduleId: "my-sample-migration-module-2" });
-    });
-}
-
-function testMigration({moduleId, migrations}: {moduleId: string, migrations: Migrations}): void {
-    migrations.register({ moduleId });
-    migrations.addMigration({
-        moduleId,
-        migration: {
-            key: `2025-11-12-${moduleId}`,
-            date: new Date("2025-11-12"),
-            func: async () => {
-                console.log("Running my sample migration from 2025-11-12");
-                return true;
-            },
-        },
-    });
-
-    migrations.addMigrations({
-        moduleId,
-        migrations: [
-            {
-                key: `2025-11-10-${moduleId}`,
-                date: new Date("2025-11-10"),
+        migrationsTyped.addMigration({
+            moduleId: MODULE_ID,
+            migration: {
+                key: `2025-11-12-${MODULE_ID}`,
+                date: new Date("2025-11-12"),
                 func: async () => {
-                    console.log("Running my sample migration from 2025-11-10");
+                    console.log("Running my sample migration from 2025-11-12 for", MODULE_ID);
                     return true;
                 },
             },
-        ],
+        });
+
+        migrationsTyped.addMigrations({
+            moduleId: MODULE_ID,
+            migrations: [
+                {
+                    key: `2025-11-10-${MODULE_ID}`,
+                    date: new Date("2025-11-10"),
+                    func: async () => {
+                        console.log("Running my sample migration from 2025-11-10 for", MODULE_ID);
+                        return true;
+                    },
+                },
+            ],
+        });
+    });
+
+    Hooks.once("migrations.ready", async (migrations: any) => {
+        const migrationsTyped = migrations as Migrations;
+        await migrationsTyped.clearAllRan({
+            moduleId: MODULE_ID,
+        });
+        await migrationsTyped.runAll({ moduleId: MODULE_ID });
     });
 }

cf017d26c3d9cc1d1ef43ebbff4c045fc51c3657
Derek Fredrickson <dfreds08@gmail.com>
2026-08-04 15:51:06 -0400

    Replace Foundry types with npm package

--- build/update-pf2e-types.ts
+++
@@ -1,4 +0,0 @@
-import { exec } from "child_process";
-import foundryConfig from "../foundryconfig.json" with { type: "json" };
-
-exec(`cd ${foundryConfig.pf2eRepoPath} && git checkout v13-dev && git pull`);
